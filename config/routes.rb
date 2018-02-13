72110b8380f9da4c26ddf6b6a156dd52d01ca591
Alexandre Gros <xelaflash@gmail.com>
2018-02-13 17:49:29 +0100

    refacto done and checkbox hidden when new record

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get "tasks", to: "tasks#index"
 
-get "tasks/new", to: "tasks#new", as: :new
-post "tasks", to: "tasks#create"
+resources :tasks, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+# get "tasks", to: "tasks#index"
+
+# get "tasks/new", to: "tasks#new", as: :new_task
+# post "tasks", to: "tasks#create"
 
 
-get "tasks/:id/edit", to: "tasks#edit", as: :edit
-patch  "tasks/:id", to: "tasks#update"
+# get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+# patch  "tasks/:id", to: "tasks#update"
 
-delete "tasks/:id", to: "tasks#destroy", as: :delete
+# delete "tasks/:id", to: "tasks#destroy"
 
-get "tasks/:id", to: "tasks#show", as: :task
+# get "tasks/:id", to: "tasks#show", as: :task
 end
